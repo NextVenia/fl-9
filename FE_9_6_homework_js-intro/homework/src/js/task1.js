859a1276013064e6370f4caf4a097db1e3d88dbe
Vitaliy Mazur <venia.prada@gmail.com>
2018-08-19 21:37:31 +0300

    Add files via upload

---
+++ FE_9_6_homework_js-intro/homework/src/js/task1.js
@@ -0,0 +1,20 @@
+// Your code goes here
+// Your code goes here
+
+const amount=parseFloat(prompt('Enter amount of money','0'));
+const discount=parseFloat(prompt('Enter a number of discount','-1'));
+let output;
+const saved=amount*discount/100;
+function priceWithDis() {
+    const result=amount-amount*discount/100;
+    return result.toFixed(2);
+}
+if(amount>0&&!isNaN(amount)&&discount>=0&&!isNaN(discount)&&discount<=100){
+    output=`Price without discount: ${+amount.toFixed(2)}
+Discount:${+discount.toFixed(2)}%
+Price with discount: ${+priceWithDis()},
+Saved:${+saved.toFixed(2)}`;
+}else {
+    output='Invalid data';
+}
+console.log(output);
---
+++ FE_9_6_homework_js-intro/homework/src/js/task2.js
@@ -0,0 +1,35 @@
+// Your code goes here
+const sideA=parseFloat(prompt('Enter side A:','0'));
+const sideB=parseFloat(prompt('Enter side B:','0'));
+const angle=parseFloat(prompt('Enter angle:','0'));
+let check=1;
+let output;
+const maxAngle=180;
+const degree=Math.PI/maxAngle*angle;
+const sideC=findSideC();
+const perimeter=sideA+sideB+sideC;
+const square=1 / 2 * sideA * sideB * Math.sin(degree);
+function validData(){
+   
+    if(isNaN(sideA)||isNaN(sideB)||isNaN(angle)){
+        check=0;
+    }
+    
+    if(sideA<0||sideB<0||angle<0||angle>maxAngle){
+        check=0;//sum of 3 angles cant be more than 180, same for 1 angle
+    }
+   return check;
+}
+function findSideC(){
+    let result=Math.pow(sideA,2)+Math.pow(sideB,2)-2*sideA*sideB*Math.cos(degree);
+    return Math.sqrt(result);
+}
+
+if(validData()===0){
+    output='Invalid data';  
+}else{
+    output=`c length:${+sideC.toFixed(2)}
+Triangle square:${+square.toFixed(2)}
+Triangle perimeter:${+perimeter.toFixed(2)}`;   
+}
+console.log(output);
